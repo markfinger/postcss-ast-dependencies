56c4b61e8b97c890dda46d130c53587ece03e8ba
Mark Finger <markfinger@gmail.com>
2016-02-22 19:28:45 +1100

    Cleanup

--- src/tests/postcss-ast-dependencies.js
+++ src/tests/postcss-ast-dependencies.js
@@ -1,4 +1,4 @@
-import {assert} from '../utils/assert';
+import {assert} from './assert';
 import postcss from 'postcss';
 import {
   postcssAstDependencies, getDependencyIdentifierFromImportRule, getDependencyIdentifiersFromDeclarationValue
